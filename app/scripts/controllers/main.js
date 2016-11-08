cc4938c53a53df884e4fa4f211469d20cfe82c2a
Yaroslav Yermilov <yermilov17@gmail.com>
2016-11-09 01:53:36 +0200

    #1. autogenerated by yeoman angular project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vuzollWebuiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vuzollWebuiApp
+ */
+angular.module('vuzollWebuiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
