a02f3395d9163c4808c35f9de3097bbdbb2eeb74
Yaroslav Yermilov <yermilov17@gmail.com>
2016-11-17 02:43:54 +0200

    #11. point self to new explore-data service

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,11 +2,11 @@
 
 angular .module('vuzollWebuiApp')
 
-        .constant("baseURL","http://138.68.145.93:3000/")
+        .constant("baseURL","http://138.68.145.93:5050/")
 
         .service('exploreFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
             this.getReport = function() {
-                return $resource(baseURL + "explore", null,  { 'update': {method:'PUT'} });
+                return $resource(baseURL, null,  { 'update': {method:'PUT'} });
             };
         }])
 ;
