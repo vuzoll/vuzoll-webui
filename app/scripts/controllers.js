b55d3d9407d259bba358730457a4e0c52c0e9170
Yaroslav Yermilov <yermilov17@gmail.com>
2016-11-23 15:39:04 +0200

    #13. more meaningful exploration report using #6 ukrainian

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -4,14 +4,14 @@ angular .module('vuzollWebuiApp')
 
         .controller('VkByVladController', ['$scope', 'exploreFactory', function($scope, exploreFactory) {
             $scope.showReport = false;
-            $scope.message = "Loading ...";
+            $scope.message = "Завантаження...";
             $scope.report = exploreFactory.getReport().get({}).$promise.then(
                 function(response) {
                     $scope.report = response;
                     $scope.showReport = true;
                 },
                 function(response) {
-                    $scope.message = "Error: " + response.status + " " + response.statusText;
+                    $scope.message = "Помилка: " + response.status + " " + response.statusText;
                 }
             );
         }])
